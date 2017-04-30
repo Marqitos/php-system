f534ffa39f2cbe0f297724c2cb89f665f4cc7205
Marcos Porto <marcosarnoso@msn.com>
2017-04-30 23:20:04 +0200

    Corrección charset a utf-8

--- InvalidOperationException.php
+++ InvalidOperationException.php
@@ -4,7 +4,7 @@ class System_InvalidOperationException
 	extends Exception {
   const COR_E_INVALIDOPERATION = 0x80131509;
 	
-	public function __contruct($message = 'Se ha producido una operación no permitida', $code = self::COR_E_INVALIDOPERATION) {
+	public function __contruct($message = 'Se ha producido una operaciÃ³n no permitida', $code = self::COR_E_INVALIDOPERATION) {
 		parent::__contruct($message, $code);
 	}
 	
