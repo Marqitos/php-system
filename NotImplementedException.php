bf1eb230b440961ed08974bfad984d08741f2cde
Marqitos <marcosarnoso@msn.com>
2019-09-16 01:35:20 +0200

    Codigos de error demasiado largos para int32

--- NotImplementedException.php
+++ NotImplementedException.php
@@ -5,7 +5,7 @@ namespace System;
 use Exception;
 
 class NotImplementedException extends Exception {
-    const E_NOTIMPL	=	0x80004001;
+    const E_NOTIMPL	= 80004001;	//0x80004001;
 	
 	public function __construct($message = 'Instrucción no implementada', $code = self::E_NOTIMPL) {
 		parent::__construct($message, $code);
