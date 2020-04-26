59b63077ca047ef847275ffc23083751112e1ea1
Marqitos <marcosarnoso@msn.com>
2020-04-26 03:12:04 +0200

    Agregación de cadenas localizadas

--- Localization/es.php
+++ Localization/es.php
@@ -2,12 +2,12 @@
 
 namespace System\Localization;
 
-
 class Resources {
 
     public const ArgumentExceptionDefaultMessage = 'Se ha especificado un argumento no válido';
     public const ArgumentNullExceptionDefaultMessage = 'El argumento no puede ser null';
     public const ArgumentOutOfRangeExceptionDefaultMessage = 'Se ha especificado un argumento fuera de rango';
+    public const ArgumentOutOfRangeExceptionIntExpected = 'Se esperaba un entero';
     public const ArgumentOutOfRangeExceptionStringExpected = 'Se esperaba una cadena';
     public const ArgumentOutOfRangeExceptionGUIDStringExpected = 'Se ha especificado una cadena GUID no válida';
     public const ArgumentOutOfRangeExceptionUrlExtected = 'Se esperaba una url válida';
@@ -16,12 +16,19 @@ class Resources {
     public const InvalidOperationExceptionDefaultMessage = 'Se ha producido una operación no permitida';
     public const IOExceptionDefaultMessage = 'Error E/S';
     public const KeyNotFoundExceptionDefaultMessage = 'La clave especificada no existe';
+    public const KeyNotFoundExceptionFormatedHttpCode = 'No se ha encontrado un codigo http con valor %s';
     public const NotImplementedExceptionDefaultMessage = 'Instrucción no implementada';
     public const NotImplementedExceptionNeedIconv = 'Se necesita la funcion "iconv"';
     public const NotSupportedExceptionDefaultMessage = 'Instrucción no soportada';
+    public const WebException304Message = 'El recurso no se ha modificado desde la última solicitud.';
+    public const WebException400Message = 'La solicitud no se puede cumplir debido a una sintaxis incorrecta';
+    public const WebException401Message = 'Acceso no autorizado, se requieren credenciales';
     public const WebException403Message = 'Acceso prohibido';
     public const WebException404Message = 'El documento no ha sido encontrado';
+    public const WebException405Message = 'Se realizó una solicitud de un recurso utilizando un método no admitido';
+    public const WebException406Message = 'El recurso solicitado solo es capaz de generar contenido no aceptable de acuerdo con los encabezados Accept enviados en la solicitud.';
     public const WebException500Message = 'Error interno de la aplicación';
-
+    public const WebException501Message = 'El servidor no reconoce el método de solicitud o carece de la capacidad para cumplir con la solicitud.';
+    public const WebException503Message = 'El servidor no está disponible actualmente (sobrecargado o en mantenimiento).';
 
 }
