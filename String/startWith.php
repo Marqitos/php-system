1e7e5670e7d914c102a1262566259b545af901ec
Marcos Porto <informatica@reprogalicia.com>
2020-11-20 18:32:10 +0100

    Optimizacion y comentarios de funciones String

--- String/startWith.php
+++ String/startWith.php
@@ -1,20 +1,39 @@
 <?php
+/**
+ * Determina si el principio de una cadena coincide con una cadena especificada.
+ *
+ * Description. Determina si el principio de una cadena coincide con una cadena especificada, 
+ * opcionalmente puede ignorar distinciones entre minusculas y mayusculas.
+ *
+ * @package System
+ * @author Marcos Porto
+ * @copyright Marcos Porto Mariño
+ * @since v0.1
+ * PHP 5 >= 5.3.0, PHP 7
+ */
 
 namespace System\String;
 
 use function mb_stripos;
 use function mb_strpos;
+use function stripos;
+use function strpos;
 
 /**
 * Determina si el principio de una cadena coincide con una cadena especificada.
 *
 * @param string $haystack (pajar) Cadena completa
 * @param string $needle (aguja) Cadena a comparar
+* @param boolean $ignoreCase Si true, ignora distinciones entre mayusculas y minusculas, por defecto false;
 * @return boolean Es true si value coincide con el principio de esta cadena; en caso contrario, es false.
 */
 function startWith($haystack, $needle, $ignoreCase = FALSE) {
     if($ignoreCase) {
-        return mb_stripos($haystack, $needle) === 0;
+        return function_exists('mb_stripos')
+            ? mb_stripos($haystack, $needle) === 0
+            : stripos($haystack, $needle) === 0;
     }
-    return mb_strpos($haystack, $needle) === 0;
+    return function_exists('mb_strpos')
+        ? mb_strpos($haystack, $needle) === 0
+        : strpos($haystack, $needle) === 0;
 }
