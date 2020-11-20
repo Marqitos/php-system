7efdac1983e7ac28d16c550bed98625cfd272748
Marcos Porto <informatica@reprogalicia.com>
2020-11-20 18:33:35 +0100

    Agregar comentarios, y corregir lanzamiento de excepciones en el constructor

--- Guid.php
+++ Guid.php
@@ -22,10 +22,17 @@ use function uniqid;
 
 class Guid {
 
-	// guid value
+	/**
+	 * Valor binario del UUID
+	 */
 	protected $raw;
 	
-	// Create a guid object with a specific value
+	/**
+	 * Crea un GUID con un valor específico
+	 * 
+	 * @param string|System\Guid $value valor inicial
+	 * @throws System\ArgumentOutOfRangeException
+	 */
 	public function __construct($value) {
 		if(is_string($value)) {
 			if(strlen($value) == 16) {
@@ -35,7 +42,7 @@ class Guid {
 					$this->raw = $this->createRaw($value);
 				} catch(Exception $e) {
 					require_once 'ArgumentOutOfRangeException.php';
-					throw new ArgumentOutOfRangeException('value', Resources::ArgumentOutOfRangeExceptionGUIDStringExpected, $value);
+					throw new ArgumentOutOfRangeException('value', Resources::ArgumentOutOfRangeExceptionGUIDStringExpected, $value, null, $e);
 				}
 			}
 		} elseif($value instanceof Guid) {
@@ -46,11 +53,23 @@ class Guid {
 		}
 	}
 	
-	// Returns a empty Guid
+	/**
+	 * Obtiene un UUID vacío
+	 * 
+	 * @return System\Guid
+	 */
 	public static function getEmpty() {
 		return new self(str_repeat(chr(0), 16));
 	}
 	
+	/**
+	 * Intenta crear un GUID a partir de una valor
+	 * 
+	 * @param mixed $parse Valor a comprobar
+	 * @param System\Guid|Exception GUID en caso de exito, o la exceción que causo que no se pudiese analizar
+	 * 
+	 * @return bool True en caso de exito
+	 */
 	public static function tryParse($value, &$guid) {
 		if($value instanceof Guid) {
 			$guid = $value;
@@ -60,23 +79,36 @@ class Guid {
 				$guid = new self($value);
 				return true;
 			} catch(Exception $e) {
+				$guid = $e;
 				return false;
 			}
 		}
 	}
 	
-	// Returns a new Guid object
+	/**
+	 * Devuelve un nuevo GUID con un valor aleatorio
+	 * 
+	 * @return System\Guid Nuevo objecto con un valor aleatorio
+	 */
 	public static function newGuid() {
 		return new self(self::getNewRaw());
 	}
 	
-	// Returns if a guid is null or empty
+	/**
+	 * Devuelve si el contenido de System\Guid es vacío, o es null
+	 * 
+	 * @return bool true en caso de estar vacío
+	 */
 	public static function isEmpty(Guid $id = null) {
 		return $id == null ||
 			$id->raw == str_repeat(chr(0), 16);
 	}
 	
-	// Returns a hex[32] string
+	/**
+	 * Obtiene el valor como una cadena hexadecimal, de 32 caracteres de longitud, toda en mayusculas
+	 * 
+	 * @return string Cadena con el valor en hexadecimal
+	 */
 	public function getHex() {
 		$cadena = '';
 		for($c = 0; $c < strlen($this->raw); $c++)
@@ -85,28 +117,48 @@ class Guid {
 	  return strtoupper($cadena);
 	}
 	
-	// Returns a string that represent a Guid
+	/**
+	 * Devuelve la representación como cadena que representa el UUID
+	 * 
+	 * @return string Cadena con el valor
+	 */
 	public function __toString()	{
 		$cadena = $this->getHex();
 		return substr($cadena,0,8).'-'.substr($cadena,8,4).'-'.substr($cadena,12,4).'-'.substr($cadena,16,4).'-'.substr($cadena,20);
 	}
 	
-	// Returns a binary[16] string 
+	/**
+	 * Obtiene el valor como una cadena binaria, de 16 caracteres de longitud
+	 * 
+	 * @return string Cadena con el valor binario
+	 */
 	public function getRaw() {
 		return $this->raw;
 	}
 	
-	// Returns if this Guid is a empty guid
+	/**
+	 * Obtiene si el elemento actual representa un UUID vacío
+	 * 
+	 * @return bool true en caso de tener un valor vacío
+	 */
 	public function getIsEmpty() {
 		return $this->raw == str_repeat(chr(0), 16);
 	}
 	
-	// Returns a new raw value
+	/**
+	 * Obtiene una nueva cadena binaria aleatoria de 16 caracteres de longitud
+	 * 
+	 * @return string Cadena binaria aleatoria
+	 */
 	protected static function getNewRaw() {
 		return self::createRaw(md5(uniqid()));
 	}
 	
-	// Returns the raw value from a guid string
+	/**
+	 * Devuelve una cadena binaria a partir de una cadena UUID
+	 * 
+	 * @return string Cadena binaria
+	 */
 	protected static function createRaw($value) {
 		$value	= str_replace('-', '', $value);
 		$raw = @hex2bin($value);
