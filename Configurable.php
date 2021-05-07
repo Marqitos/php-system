ebd3f52fe1b97a90502730558b25de009e5760e5
Marcos Porto <informatica@reprogalicia.com>
2021-05-07 13:51:16 +0200

    Añadimos strict_types, y eliminamos comprobaciones de tipo mediante código

--- Configurable.php
+++ Configurable.php
@@ -1,8 +1,16 @@
-<?php
+<?php declare(strict_types = 1 );
+/**
+ * Implementa un objeto con opciones de configuración
+ *
+ * @package System
+ * @author Marcos Porto
+ * @copyright 2021, Marcos Porto
+ * @since v0.4
+ */
 
 namespace System;
 
-use System\ArgumentOutOfRangeException;
+use System\Collections\KeyNotFoundException;
 use System\Configurable\ConfigurableInterface;
 use System\Localization\Resources;
 
@@ -10,8 +18,6 @@ use function array_key_exists;
 use function array_replace_recursive;
 use function call_user_func;
 use function is_array;
-use function is_callable;
-use function is_string;
 use function strtolower;
 
 require_once 'System/Configurable/ConfigurableInterface.php';
@@ -21,13 +27,14 @@ require_once 'System/Configurable/ConfigurableInterface.php';
  */
 abstract class Configurable implements ConfigurableInterface {
 
+	protected const EVENT_CHANGING 	= 'onChanging';
+	protected const EVENT_CHANGED	= 'onChanged';
 	/// Campos
 	protected $options;
 	private $callbacks = [
-		'onChanging' => [],
-		'onChanged'	 => []  
+		self::EVENT_CHANGING => [],
+		self::EVENT_CHANGED	 => []  
 	];
-	private $onOptionChangedCallbacks = [];
 
 	/**
 	* Constructor
@@ -37,41 +44,40 @@ abstract class Configurable implements ConfigurableInterface {
 	public function __construct(array $options) {
 		global $environment;
 		$this->options = $this->getDefaultOptions($environment->getStatus());
-		foreach($options as $key => $value)
+		foreach($options as $key => $value) {
 			$this->setOption($key, $value);
+		}
 	}
 
 	/**
-	* Registra una función para que sea llamada cuando se vaya a producir un cambio en la configuración
+	* Registra una función para que sea llamada cuando se produzca el evento indicado
 	*
-	* @param callback $callback
+	* @param string $eventName Nombre predefinido del evento
+	* @param callback $callback Función que se ejecutará
+	* @throws KeyNotFoundException Si el nombre del evento no es válido
 	* @return void
 	*/
-	protected function registerOptionChanging(callable $callback) {
-		$this->callbacks['onChanging'][] = $callback;
+	protected function registerEvent(string $eventName, callable $callback) : void {
+		if(!array_key_exists($eventName, $this->callbacks)) {
+			require_once 'System/Collections/KeyNotFoundException.php';
+			throw new KeyNotFoundException(sprintf(Resources::KEY_NOT_FOUND_EXCEPTION_NO_EVENT_FORMAT, $eventName));
+		}
+		$this->callbacks[$eventName][] = $callback;
 	}
 
-	/**
-	* Registra una función para que sea llamada cuando se produzca un cambio en la configuración
-	*
-	* @param callback $callback
-	* @return void
-	*/
-	protected function registerOptionChanged(callable $callback) {
-		$this->callbacks['onChanged'][] = $callback;
-	}
-
-	private function raiseOptionChanged($optionName) {
-		foreach($this->callbacks['onChanged'] as $callback)
+	private function raiseOptionChanged($optionName) : void {
+		foreach($this->callbacks[self::EVENT_CHANGED] as $callback) {
 			call_user_func($callback, $optionName);
+		}
 	}
 
-	private function raiseOptionChanging($optionName, $value) {
+	private function raiseOptionChanging($optionName, $value) : bool {
 		$result = true;
-		foreach($this->callbacks['onChanging'] as $callback) {
+		foreach($this->callbacks[self::EVENT_CHANGING] as $callback) {
 			$result = call_user_func($callback, $optionName, $value);
-			if($result != true)
+			if(!$result) {
 				break;
+			}
 		}
 		return $result;
 	}
@@ -79,25 +85,22 @@ abstract class Configurable implements ConfigurableInterface {
 	// Miembros de System\Configurable\ConfigurableInterface
 
 	/**
-	* Establece una opción de configuración
+	* Establece una opción de configuración,
+	* solo se pueden establecer claves de configuración ya existentes
 	*
 	* @param  string $name
 	* @param  mixed  $value
-	* @throws System\ArgumentOutOfRangeException
 	* @return void
 	*/
-	public function setOption($name, $value) : void {
-		if (!is_string($name)) {
-			require_once 'System/ArgumentOutOfRangeException.php';
-			throw new ArgumentOutOfRangeException('name', Resources::ArgumentOutOfRangeExceptionStringExpected, $name);
-		}
+	public function setOption(string $name, $value) : void {
 		$name = strtolower($name);
 		if (array_key_exists($name, $this->options) &&
 			$this->options[$name] !== $value &&
 			$this->raiseOptionChanging($name, $value)) {
 			
-			if(is_array($this->options[$name]) && is_array($value))
+			if(is_array($this->options[$name]) && is_array($value)) {
 				$value = array_replace_recursive($this->options[$name], $value);
+			}
 			$this->options[$name] = $value;
 			$this->raiseOptionChanged($name);
 		}
--- Configurable/ConfigurableInterface.php
+++ Configurable/ConfigurableInterface.php
@@ -1,20 +1,31 @@
-<?php
+<?php declare(strict_types = 1 );
+/**
+ * Representa un objeto con opciones de configuración
+ *
+ * @package System
+ * @author Marcos Porto
+ * @copyright 2021, Marcos Porto
+ * @since v0.4
+ */
 
 namespace System\Configurable;
 
 /**
- * Representa un objeto con opciones de configuración, mediante clave-valor
+ * Representa un objeto con opciones de configuración,
+ * mediante clave-valor
  */
 interface ConfigurableInterface {
     /**
      * Establece el valor de un parametro
+     * 
      * @param string $key Clave de configuración
      * @param mixed $value Valor de configuración
      * @return void
      */
-    public function setOption($key, $value) : void;
+    public function setOption(string $key, $value) : void;
     /**
      * Obtiene toda la configuración
+     * 
      * @return array Datos de configuración actuales
      */
     public function getOptions() : array;
