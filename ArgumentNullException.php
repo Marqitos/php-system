a0eb6b37197a20a5ce3656c9f228a3d18c946b98
Marcos Porto (sgt) <marcosarnoso@msn.com>
2021-08-31 21:20:18 +0200

    Agregamos funciones de comprobación de argumentos en arrays a ArgumentNullException

--- ArgumentNullException.php
+++ ArgumentNullException.php
@@ -25,18 +25,38 @@ require_once 'ArgumentException.php';
  */
 class ArgumentNullException extends ArgumentException {
 
-	public function __construct($paramName, $message = Resources::ArgumentNullExceptionDefaultMessage, $code = HResults::E_POINTER, Throwable $previous = null) {
+	public function __construct(string $paramName, string $message = Resources::ArgumentNullExceptionDefaultMessage, int $code = HResults::E_POINTER, Throwable $previous = null) {
 		parent::__construct($paramName, $message, $code, $previous);
 	}
 
 	/**
 	 * Comprueba el valor de una variable, y en caso de que sea null, lanza una excepción
+	 * 
+	 * @param string $paramName Nombre del parametro a comprobar
+	 * @param mixed $value Variable a comprobar
+	 * @throws ArgumentNullException En caso de que $value sea null
 	 */
-	public static function validate($paramName, $value) {
+	public static function validate(string $paramName, $value) {
 		if($value === null) {
 			throw new self($paramName);
 		}
-		return true;
+	}
+
+	/**
+	 * Comprueba el valor de un elemento dentro de un array,  y en caso de que sea null, lanza una excepción
+	 * 
+	 * @param string $paramName Nombre del parametro a comprobar
+	 * @param array $value Array donde buscar el elemento
+	 * @param mixed $key Indice del elemento en el array
+	 * @return mixed El valor de $value[$key]
+	 * @throws ArgumentNullException En caso de que $value[$key] sea null
+	 */
+	public static function validateArray(string $paramName, array $value, $key) {
+		if(isset($value[$key]) ||
+		   $value[$key] === null ) {
+			throw new self($paramName . '[' . $key . ']');
+		}
+		return $value[$key];
 	}
 	
 }
