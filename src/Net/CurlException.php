53368ead405282b2a08999f14cbb7ff1eb72eb4e
Marcos Porto Mariño <lib-system@marcospor.to>
2025-04-14 22:13:16 +0200

    Update CurlException

--- src/Net/CurlException.php
+++ src/Net/CurlException.php
@@ -6,10 +6,15 @@ use Exception;
 use function curl_error;
 use function curl_errno;
 
-class CurlException extends Exception {
+class CurlException extends RuntimeException {
 
-    public function __contruct($curlResource) {
-        parent::__construct(curl_error($curlResource), curl_errno($curlResource));
+    public static function validate(CurlHandle|false $handle) {
+        if ($handle === false) { // No se ha creado el manejador
+            throw new RuntimeException;
+        } else {
+            if (curl_errno($handle) !== 0) { // Se ha producido un error
+                throw new self(curl_error($handle), curl_errno($handle));
+            }
+        }
     }
-
 }
